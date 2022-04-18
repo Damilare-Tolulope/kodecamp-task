743e821c5ac4cb356cdf25d7efb5cf39adb5a37d
Damilare <54348429+Damilare-Tolulope@users.noreply.github.com>
2022-04-18 13:40:14 +0100

    Education

---
+++ src/components/Education.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Education = () => {
+  return (
+    <div className="education">
+          <h2>Education</h2>
+          <div className="edu-1">
+            <div className='date'>
+                2018 - present
+            </div>
+            <div className='role'>
+              <h2>Federal University of Technology, Akure</h2>
+              <p><i>Akure, Ondo State</i></p>
+              <ul>
+                <li>Study Computer Science</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+  )
+}
+
+export default Education
