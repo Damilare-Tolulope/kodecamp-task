889bed013da4780e8e2d36b3ba302e79495ad50c
Damilare <54348429+Damilare-Tolulope@users.noreply.github.com>
2022-04-18 13:40:47 +0100

    Skills

---
+++ src/components/Skills.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div className="skills">
+    <h2>Skills</h2>
+        <ul>
+          <li>Web Development</li>
+          <li>Communication</li>
+          <li>Teamwork</li>
+        </ul>
+  </div>
+  )
+}
+
+export default Skills
