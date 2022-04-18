805cfe1f6289ab8746e9c941456a653c1b1d0cee
Damilare <54348429+Damilare-Tolulope@users.noreply.github.com>
2022-04-18 13:40:24 +0100

    Experience

---
+++ src/components/Experience.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <div className="experience">
+        <h2>Experience</h2>
+        <div className="exp-1">
+        <div className='date'> 12/2021 </div>
+        <div className='role'>
+            <h2>Sidehustle Internship</h2>
+            <p><i>Remote</i></p>
+            <ul>
+            <li>Learnt about reactjs</li>
+            <li>Leant how to collaborate using tools like git and github</li>
+            </ul>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Experience
