c8562cfe9d33428aaf4ce2426ca1bd7df306acda
Damilare <54348429+Damilare-Tolulope@users.noreply.github.com>
2022-04-18 13:40:39 +0100

    Languages

---
+++ src/components/Language.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Language = () => {
+  return (
+    <div className="language">
+        <h2>Languages</h2>
+            <ul>
+                <li>English</li>
+                <li>Yoruba</li>
+            </ul>
+    </div>
+  )
+}
+
+export default Language
